52dd6f10ab0a33f4fd3abced0743b2d7e49cbc28
Khishigdari <nbhishgee22@gmail.com>
2025-08-04 12:33:32 +0800

    bodlogo

--- index.js
+++ index.js
@@ -2,12 +2,43 @@
 // const b = 4;
 // console.log(a + b);
 
-import { color } from "console-log-colors";
-console.log(color.blue("Hello World"));
+// ============
 
-import _ from "lodash";
-const max = _.max([1, 2, 3, 5]);
-console.log(color.red(max));
+// import { color } from "console-log-colors";
+// console.log(color.blue("Hello World"));
 
-import chalk from "chalk";
-console.log(chalk.blue("Hello") + chalk.green(" World") + chalk.red("!"));
+// import _ from "lodash";
+// const max = _.max([1, 2, 3, 5]);
+// console.log(color.red(max));
+
+// import chalk from "chalk";
+// console.log(chalk.blue("Hello") + chalk.green(" World") + chalk.red("!"));
+
+// ============
+let s = "Hello World";
+
+const lengthOfLastWord = (s) => {
+  // 1. find the last word
+  // 2. return last word length
+
+  // buh char log hiideg for loop bich
+  //  if (char !== " ") console.log(char)
+  // if(char === " ") return
+
+  const arr = s.trim("").split(" ");
+  return arr[arr.length - 1].length;
+
+  //   let latsWordLength = 0;
+  //   let isFindLastWord = false;
+  //   for (i = s.length - 1; i >= 0; i--) {
+  //     const char = s[i];
+  //     if (char !== " ") {
+  //       isFindLastWord = true;
+  //       latsWordLength++;
+  //       console.log(s[i]);
+  //     } else {
+  //       if (isFindLastWord) {
+  //         return latsWordLength;
+  //       }
+  //     }
+};
